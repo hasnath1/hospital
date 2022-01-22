c57acb207807e28bd938aee64917be6047cd053e
hasnath1 <abdullahaknd297@gmail.com>
2022-01-22 12:55:18 +0600

    commit

---
+++ data/fakeData.js
@@ -0,0 +1,35 @@
+const data = {
+  name: "Hasnath Abdullah",
+
+  socialMedia: {
+    fb_link: "#",
+    tw_link: "#",
+    instagram_link: "#",
+  },
+
+  img_link: "/image/cool_man.png",
+
+  info: {
+    field: "Neurologist",
+    email: "example@gmail.com",
+    patients: "500+",
+    experience: "10+ years",
+    phone: "+880-173-23-34024",
+    duty: "9:00 am - 5:00 pm",
+    address: "Rakuail-Gailtal Road, Kishoreganj Sador , Dhaka , Bangladesh.",
+  },
+
+  aboutMe:
+    "I am a web developer with centuries of experience. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with d",
+
+  skills: [
+    "Massachusetts Institute of Technology",
+    "Mechanical Engineering",
+    "MBBS",
+    "FCPS",
+    "Electrical Engineering",
+  ],
+};
+
+export { data };
+export default data;
